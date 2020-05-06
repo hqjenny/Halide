7f6488d95e8a0ba32f2c561745b60fd7f82bc71a
Jenny Huang <hqjenny@hotmail.com>
2020-05-06 01:34:04 -0700

    Add result parser

---
+++ apps/autoscheduler/parse_out.py
@@ -0,0 +1,32 @@
+import re
+
+postfix = ['1', '2', '3', '4', '5', '6', '7', '8', '10']
+#postfix = ['1']
+print("depth\t#options\tManually-tuned\tMCTS-tuned")
+for p in postfix:
+    #print('parse '+ 'mcts_'+p)
+    with open('mcts_'+p,'r') as f:
+        data = f.readlines()
+    total_options = 0
+    manual_time =  -1
+    mcts_time =  -1
+    
+    for line in data:
+        m  = re.match(r"---------JENNY tile_options (\d+)-----", line)
+        if m:
+            options = int(m.group(1))
+            total_options += options
+            continue
+
+        m  = re.match(r"Manually-tuned time: (\S+)ms", line)
+        if m:
+            manual_time = float(m.group(1))
+            continue
+
+
+        m  = re.match(r"Auto-scheduled time: (\S+)ms", line)
+        if m:
+            mcts_time = float(m.group(1))
+            continue
+
+    print("{}\t{}\t{}\t{}".format(p, total_options, manual_time, mcts_time))
